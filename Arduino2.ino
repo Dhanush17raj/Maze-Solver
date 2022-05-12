75275aae91d7afa31ed175943359fb1fb8a83070
Abhijith Krishna G <85012319+Abhi-Git2208@users.noreply.github.com>
2022-05-12 09:17:58 +0530

    Update Arduino2.ino

--- Arduino2.ino
+++ Arduino2.ino
@@ -44,8 +44,15 @@ void loop(){
   pass;
 }
 
-void sonar(int sensor){
-  pass;
+void sonar(int trigger, int echo){
+  digitalWrite(trigger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger, LOW);
+  duration = pulseIn(echo, HIGH);
+  distance = duration*0.034/2;
+  return(distance);
 }
 
 void forward(){
